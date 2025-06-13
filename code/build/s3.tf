1e426bfb9a0fd5c3e1e91dc4e40fbd401bec4403
Alexander Tarkhov <191038348+tarkhovsasha@users.noreply.github.com>
2025-06-13 04:19:32 +0300

    Update s3.tf

--- code/build/s3.tf
+++ code/build/s3.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "dev_s3_ins" {
+resource "aws_s3_bucket" "dev_s3" {
   bucket_prefix = "dev-"
 
   tags = {
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "dev_s3_ins" {
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "dev_s3_ins" {
+resource "aws_s3_bucket_ownership_controls" "dev_s3" {
   bucket = aws_s3_bucket.dev_s3.id
   rule {
     object_ownership = "BucketOwnerPreferred"
